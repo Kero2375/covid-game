85ec23611b1688a5ed0a13459848638073c73ee8
Simone <simoneinnocente98@gmail.com>
2020-11-30 18:33:46 +0100

    Sistemato

--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -6,6 +6,7 @@ public class Movement : MonoBehaviour{
 
     public float movementSpeed;
     public SwipeManager swipe;
+    private int desiredLane = 1;
 
     void Start()    {
         
@@ -13,49 +14,26 @@ public class Movement : MonoBehaviour{
 
     // Update is called once per frame
     void Update(){
-        /*if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey("w")){
-            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed *2.5f;
-        }else if(Input.GetKey("w") && !Input.GetKey(KeyCode.LeftShift)){
-            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
-        }else if (Input.GetKey("s"))
-        {
-            transform.position -= transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
-        }*/
+        transform.position += transform.TransformDirection(Vector3.forward) * Time.fixedDeltaTime * 1.5f;
 
-
-        /* if (Input.GetKey("w") && !Input.GetKey("s"))
-         {
-             transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed*6.5f;
-
-         }
-         else if (Input.GetKey("s") && !Input.GetKey("w"))
-         {
-             transform.position -= transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed*6.5f;
-         }*/
-        if (swipe.SwipeUp)
-        {
+        /*if (swipe.SwipeUp || (Input.GetKey("w") && !Input.GetKey("s"))){
             transform.position += transform.TransformDirection(Vector3.forward) * movementSpeed * 6.5f;
 
-        }
-        else if (swipe.SwipeDown)
-        {
-            transform.position -= transform.TransformDirection(Vector3.forward)  * movementSpeed * 6.5f;
-        }
-
-        /*if (Input.GetKey("a") && !Input.GetKey("d")){
-            transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed * 2.5f;
-        }
-        else if (Input.GetKey("d") && !Input.GetKey("a")){
-            transform.position -= transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed * 2.5f;
+        }else if (swipe.SwipeDown || (Input.GetKey("s") && !Input.GetKey("w"))){
+            transform.position -= transform.TransformDirection(Vector3.forward) * movementSpeed * 6.5f;
         }*/
 
-        if (swipe.SwipeLeft){
-           transform.position += transform.TransformDirection(Vector3.left) * movementSpeed * 2.5f;
-       }
-       else if (swipe.SwipeRight){
-           transform.position -= transform.TransformDirection(Vector3.left)  * movementSpeed * 2.5f;
-       }
+        if (swipe.SwipeLeft || (Input.GetKey("a") && !Input.GetKey("d"))){
+            if (desiredLane != 0){
+                transform.position += transform.TransformDirection(Vector3.left) * movementSpeed * 2.5f;
+                desiredLane--;
+            }
 
-
+        }else if (swipe.SwipeRight || (Input.GetKey("a") && !Input.GetKey("d"))){
+            if (desiredLane != 2){
+                transform.position -= transform.TransformDirection(Vector3.left) * movementSpeed * 2.5f;
+                desiredLane++;
+            }
+        }
     }
 }
---
+++ Assets/Script/TileManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour{
+
+    public GameObject[] tilePrefabs;
+    public float zSpawn = 0;
+    private float tileLength = 23;
+    private int numberOfTiles = 3;
+
+    private List<GameObject> activeTiles = new List<GameObject>();
+
+    public Transform playerTransform;
+
+    void Start(){
+        for(int i = 0; i < numberOfTiles; i++){
+            SpawnTile(0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if(playerTransform.position.z - 23 > zSpawn - (numberOfTiles * tileLength)){
+            SpawnTile(0);
+            DeleteTile();
+        }
+    }
+
+    private void DeleteTile(){
+        Destroy(activeTiles[0]);
+        activeTiles.RemoveAt(0);
+    }
+
+    public void SpawnTile(int tileIndex){
+        GameObject go = Instantiate(tilePrefabs[tileIndex], transform.forward * zSpawn , transform.rotation);
+        activeTiles.Add(go);
+        zSpawn += tileLength;
+    }
+}
