f2bd5237a05051bad6e4a06ccc5d3ad7f5c1186a
Federico Carboni <f.carboni98@gmail.com>
2020-12-04 12:38:58 +0100

    Generazione assembramenti
    + fix vari nel movimento

---
+++ Assets/Script/SchivaAssembramenti/Movement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    public float initSpeed;
+    public SwipeManager swipeManager;
+    int lane = 1;
+
+    private float speed;
+    private bool moving = false;
+    private bool jumping = false;
+
+    void Start() {
+        speed = initSpeed;
+    }
+
+    void Update() {
+        gameObject.transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
+        SwipeManager.Direction dir = swipeManager.GetDirection();
+
+        if(!moving)
+            switch (dir) {
+                case SwipeManager.Direction.Left:
+                    if(lane != 0) {
+                        gameObject.transform.Rotate(new Vector3(0, -45, 0));
+                        moving = true;
+                        lane--;
+                    }
+                    break;
+                case SwipeManager.Direction.Right:
+                    if(lane != 2) {
+                        gameObject.transform.Rotate(new Vector3(0, 45, 0));
+                        moving = true;
+                        lane++;
+                    }
+                    break;
+                case SwipeManager.Direction.Up:
+                    if(!jumping)
+                        Jump();
+                    break;
+            }
+
+        float posX = gameObject.transform.position.x;
+        float rotY = gameObject.transform.rotation.y;
+        //Se sto andando alla lane n e ci sono arrivato (pos </> qualcosa)
+        if ((lane == 0 && posX < -2.5F) ||
+            (lane == 1 && (
+                (posX < 0 && rotY < 0F) ||  //sto arrivando da destra
+                (posX > 0 && rotY > 0F))) || //sto arrivando da sinistra
+            (lane == 2 && posX > 2.5F)) {
+            //Ferma il movimento
+            gameObject.transform.rotation = new Quaternion(0, 0, 0, 0);
+            moving = false;
+        }
+    }
+
+    void Jump() {
+        gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, 300, 0));
+        jumping = true;
+        gameObject.GetComponent<Animator>().SetBool("jumping", jumping);
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        jumping = false;
+        gameObject.GetComponent<Animator>().SetBool("jumping", jumping);
+    }
+
+    
+}
---
+++ Assets/Script/SchivaAssembramenti/SpawnManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+    public GameObject[] obstaclePrefabs;
+    List<GameObject> obstacles = new List<GameObject>();
+    public Transform playerTransform;
+
+    public float minDistance = 60; //minima distanza dello spawn dal player
+    float spawningRate = 3F;
+    float timer;
+    float timerIncrement = 20;
+
+    void Start() {
+        //timer = spawningRate;
+        InvokeRepeating("Spawn", 0, spawningRate);
+    }
+
+    void Update() {
+        //timer -= Time.deltaTime;
+        //timerIncrement -= Time.deltaTime;
+
+        //if(timer <= 0F) {
+        //    Spawn();
+        //    timer = spawningRate;
+        //}
+
+        //if(timerIncrement <= 0F && spawningRate > 1) {
+        //    spawningRate -= 0.5F;
+        //    timerIncrement = 20;
+        //}
+        
+        if(obstacles.Count > 0 && 
+            obstacles[0].transform.position.z < playerTransform.position.z - 20) {
+            Destroy(obstacles[0]);
+            obstacles.RemoveAt(0);
+        }
+    
+    }
+
+    void Spawn() {
+        GameObject newObstacle = Instantiate(obstaclePrefabs[0]);
+
+        float randomDistance = minDistance + Random.Range(0, 30);
+        float[] lanes = { -2.5F, 0F, 2.5F };
+        float randomLane = lanes[Random.Range(0, 2)];
+
+        Vector3 pos = new Vector3(
+            randomLane,
+            playerTransform.position.y,
+            playerTransform.position.z + randomDistance);
+
+        newObstacle.transform.position = pos;
+        obstacles.Add(newObstacle);
+    }
+}
