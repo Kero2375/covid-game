2a61538db7298a81973666a83c29469b984270f6
Simone <simoneinnocente98@gmail.com>
2020-12-10 19:51:57 +0100

    Aggiunta della morale

--- Assets/Script/SchivaAssembramenti/GameManager.cs
+++ Assets/Script/SchivaAssembramenti/GameManager.cs
@@ -5,18 +5,25 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
+
+
     private int life;
+    private int points;
     private GameObject[] hearts = new GameObject[3];
+
+    private List<string> morals = new List<string>();
+    private int moralIndex;
     public GameObject gameOver;
 
-    private int points;
 
     private void Start() {
+        PopulateMorals();
+        moralIndex = Random.Range(0, morals.Count);
         Time.timeScale = 1;
         for (int i = 0; i < 3; i++) {
             hearts[i] = GameObject.Find("Heart " + i);
         }
-        life = 3;
+        life = 1;
         points = 0;
     }
 
@@ -24,7 +31,10 @@ public class GameManager : MonoBehaviour {
         //Se perdo le vite fermo il gioco
         if (life == 0) {
             Time.timeScale = 0;
+            
             gameOver.SetActive(true);
+            gameOver.transform.GetChild(1).GetComponent<Text>().text = morals[moralIndex];
+
         }
     }
 
@@ -41,4 +51,16 @@ public class GameManager : MonoBehaviour {
         points++;
         gameOver.transform.GetChild(2).GetComponent<Text>().text = "Punti guadagnati: " + points;
     }
+
+    private void PopulateMorals() {
+        morals.Add("Ricorda di evitare i contatti ravvicinati mantenendo la distanza di almeno un metro");
+        morals.Add("Mi raccomando, indossa sempre la mascherina, serve per limitare la diffusione");
+        morals.Add("Indossa la mascherina correttamente coprendo bene mento, bocca e naso");
+        morals.Add("Evita abbracci e strette di mano");
+        morals.Add("Non toccarti occhi, naso e bocca con le mani");
+        morals.Add("Evita i luoghi affollati");
+        morals.Add("Se hai sintomi simili all'influenza, resta a casa e contatta il tuo medico");
+
+    }
+
 }
