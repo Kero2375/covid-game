d82d7f41bd64dddaaa6a1d1fbc98d52e98711731
Federico Carboni <f.carboni98@gmail.com>
2020-12-17 11:04:24 +0000

    Teletrasporto

--- Assets/Script/SchivaAssembramenti/Movement.cs
+++ Assets/Script/SchivaAssembramenti/Movement.cs
@@ -20,25 +20,34 @@ public class Movement : Swipable {
     override protected void Update() {
         base.Update();
         Run();
-        StopPlayerOnLane();
     }
 
     //Corsa del giocatore
     private void Run() {
-        gameObject.transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
+        transform.position += new Vector3(0, 0, speed) * Time.deltaTime;
     }
 
     //Controllo giocatore
     override protected void OnLeftSwipe() {
         if (moving == Dir.None && lane.ToLeft()) {
-            gameObject.transform.Rotate(new Vector3(0, -45, 0));
-            moving = Dir.Left;
+            //gameObject.transform.Rotate(new Vector3(0, -45, 0));
+            transform.position = new Vector3(
+                lane.GetLane(),
+                transform.position.y,
+                transform.position.z);
+            //moving = Dir.Left;
+            //dir = -1;
         }
     }
     override protected void OnRightSwipe() {
         if (moving == Dir.None && lane.ToRight()) {
-            gameObject.transform.Rotate(new Vector3(0, 45, 0));
-            moving = Dir.Right;
+            //gameObject.transform.Rotate(new Vector3(0, 45, 0));
+            transform.position = new Vector3(
+                lane.GetLane(),
+                transform.position.y,
+                transform.position.z);
+            //moving = Dir.Right;
+            //dir = 1;
         }
     }
     override protected void OnUpSwipe() {
@@ -53,18 +62,25 @@ public class Movement : Swipable {
     }
 
     //Controllo del movimento
-    private void StopPlayerOnLane() {
+    /*IEnumerator StopPlayerOnLane() {
+        yield return new WaitForSecondsRealtime(.3F);
+        gameObject.transform.rotation = new Quaternion();
+        moving = Dir.None;
+        dir = 0;
+
         float posX = gameObject.transform.position.x;
         //Se il giocatore si muove verso dx/sx e raggiunge la lane corretta
         if (moving == Dir.Right && posX >= lane.GetLane()) {
             //Resetto la rotazione e l'indicatore della direzione
             gameObject.transform.rotation = new Quaternion();
             moving = Dir.None;
+            dir = 0;
         } else if (moving == Dir.Left && posX <= lane.GetLane()) {
             gameObject.transform.rotation = new Quaternion();
             moving = Dir.None;
+            dir = 0;
         }
-    }
+    }*/
 
     //Collisione con la strada
     void OnCollisionEnter(Collision collision) {
