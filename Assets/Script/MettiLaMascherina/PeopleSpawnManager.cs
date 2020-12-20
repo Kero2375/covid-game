4997024ab5be656e76c06958fa2ac1b874482f38
Simone <simoneinnocente98@gmail.com>
2020-12-20 16:09:42 +0100

    Aggiunta strada con 5 lane, aggiunto personaggio singolo, creata scena

---
+++ Assets/Script/MettiLaMascherina/PeopleSpawnManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeopleSpawnManager : MonoBehaviour{
+
+    public GameObject[] peoplePrefabs;
+
+    private Spawner spawner = new Spawner();
+
+
+    //Altezza del suolo y = 0.6
+    //Posizione player = 0 0 0 
+    //cordinata x centro lane -6 -3 0 3 +6
+    void Start(){
+        float x = -6F;
+        for(int i = 0; i < 5; i++) {
+            spawner.Spawn(peoplePrefabs).SetPosition(x, 0.6F, 10F).Rotate(0,180,0);
+            x += 3;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+}
