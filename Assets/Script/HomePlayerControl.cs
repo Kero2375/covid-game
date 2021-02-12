2d90daf495902425c4f53445c81a413bda75ac5d
Federico Carboni <f.carboni98@gmail.com>
2021-02-12 15:07:36 +0100

    Aggiunta persona rotante

---
+++ Assets/Script/HomePlayerControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomePlayerControl : MonoBehaviour {
+    public float rotationFactor;
+    public Texture[] skins;
+    private Material material;
+
+    private void Start() {
+        material = GetComponentInChildren<SkinnedMeshRenderer>().material;
+        InvokeRepeating("ChangeTexture", 0, 10);
+        SaveData.ApplyMask(ref material);
+    }
+
+    void Update() {
+        gameObject.transform.Rotate(new Vector3(0, rotationFactor*Time.deltaTime, 0));
+    }
+
+    private void ChangeTexture() {
+        material.SetTexture("_MainTex", skins[Random.Range(0, skins.Length)]);
+    }
+}
