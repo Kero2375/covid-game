b4aea48dd9c5e643c64c8cb92b0b4d5cae9fc00e
Simone <simoneinnocente98@gmail.com>
2021-02-14 10:04:13 +0100

    Aggiunta classe CoinManager (da finire)

---
+++ Assets/Script/BackOff/CoinsManagerBackOff.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinsManagerBackOff : MonoBehaviour{
+
+    public GameObject[] coinPrefabs;
+    public GameManagerBackOff gameManager;
+
+    private Spawner spawner = new Spawner();
+    public MeshRenderer floor;
+
+    void Start(){
+        InvokeRepeating("Spawn", 0, 5F);
+    }
+
+    void Spawn() {
+        Plane plane = new Plane(Vector3.up, Vector3.zero);
+
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(
+            Random.Range(0, 2),
+            Random.value,
+            0));
+
+        plane.Raycast(ray, out float distance);
+
+        Vector3 pos = ray.GetPoint(distance);
+
+        spawner
+            .Spawn(coinPrefabs, "Coin")
+            .SetPosition(
+                pos.x,
+                null,
+                pos.z)
+            .GetObject()
+            .GetComponent<PeopleMovements>()
+            .floor = this.floor;
+
+    }
+}
